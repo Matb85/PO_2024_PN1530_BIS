a0141e96ce091498144e6df9befde2e456bf4315
matb85 <mateuszbis85@gmail.com>
2024-10-21 19:41:52 +0200

    feat: use Vector2d in World.java

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -1,6 +1,7 @@
 package agh.ics.oop;
 
 import agh.ics.oop.model.MoveDirection;
+import agh.ics.oop.model.Vector2d;
 
 import java.util.Arrays;
 
@@ -12,6 +13,12 @@ public class World {
         MoveDirection[] directions = OptionsParser.parseStringArray(args);
         System.out.println(Arrays.toString(directions));
         System.out.println("system zakończył działanie");
+
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
     }
 
     private static void run(MoveDirection[] args){
