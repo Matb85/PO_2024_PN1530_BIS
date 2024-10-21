b0bec5f18c2ba77446b3501486b9d1b26d427619
matb85 <mateuszbis85@gmail.com>
2024-10-21 19:41:19 +0200

    test: add test cases for OptionsParser

---
+++ src/test/java/agh/ics/oop/OptionsParserTest.java
@@ -0,0 +1,18 @@
+package agh.ics.oop;
+
+import agh.ics.oop.model.MoveDirection;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OptionsParserTest {
+
+    @Test
+    void parseStringArray() {
+        final var case1 = new MoveDirection[]{MoveDirection.FORWARD, MoveDirection.BACKWARD, MoveDirection.RIGHT, MoveDirection.LEFT};
+        assertArrayEquals( case1, OptionsParser.parseStringArray(new String[]{"f", "b", "r", "l"}));
+
+        final var case2 = new MoveDirection[]{MoveDirection.FORWARD, MoveDirection.BACKWARD, MoveDirection.RIGHT, MoveDirection.LEFT};
+        assertArrayEquals( case2, OptionsParser.parseStringArray(new String[]{"f","test", "b","should not be here", "r", "l"}));
+     }
+}
