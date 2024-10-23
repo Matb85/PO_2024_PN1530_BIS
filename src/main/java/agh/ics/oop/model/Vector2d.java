3b0bed727ddc34c20c8428a41f42b80aa641d336
matb85 <mateuszbis85@gmail.com>
2024-10-23 18:46:43 +0200

    feat: override the hashCode method to Vector2d

--- src/main/java/agh/ics/oop/model/Vector2d.java
+++ src/main/java/agh/ics/oop/model/Vector2d.java
@@ -1,5 +1,7 @@
 package agh.ics.oop.model;
 
+import java.util.Objects;
+
 public class Vector2d {
     final private int x;
     final private int y;
@@ -54,6 +56,11 @@ public class Vector2d {
         if (this == other) return true;
         if (!(other instanceof Vector2d that)) return false;
 
-        return this.x == that.x && this.y == that.y;
+        return x == that.x && y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
