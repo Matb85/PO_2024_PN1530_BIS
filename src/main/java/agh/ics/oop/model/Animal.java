d0f763521851aeb0e11355cbe84bb12b8e1f536a
matb85 <mateuszbis85@gmail.com>
2024-11-11 19:55:20 +0100

    refactor: remove upperRight and lowerLeft from Animal.java

--- src/main/java/agh/ics/oop/model/Animal.java
+++ src/main/java/agh/ics/oop/model/Animal.java
@@ -4,9 +4,6 @@ public class Animal {
     private MapDirection orientation = MapDirection.NORTH;
     private Vector2d position = new Vector2d(2,2);
 
-    private final Vector2d upperRight = new Vector2d(4,4);
-    private final Vector2d lowerLeft = new Vector2d(0,0);
-
     public Animal(){
     }
 
